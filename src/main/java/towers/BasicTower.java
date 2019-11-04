9bd7a315c345be3b060c02e51369b7a03dbe88b7
dominiclandolf <dominiclandolf@10.89.73.154>
2019-11-04 13:46:54 +0800

    Resolved conflicts.

--- src/main/java/towers/BasicTower.java
+++ src/main/java/towers/BasicTower.java
@@ -2,52 +2,82 @@ package towers;
 import monsters.*;
 import general.*;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.Math; 
-import javax.imageio.ImageIO;
 
 
 public class BasicTower {
-	Game game;
-	int range;
-	int attackPower;
-	int x;
-	int y;
-	int cost;
-	BufferedImage img;
+	private int range;
+	private int attackPower;
+	private int x;
+	private int y;
+	private int cost;
+	private String img;
 	
-	public BasicTower(int x, int y, Game game) throws IOException {
-		this.game = game;
+	public BasicTower(int x, int y){
 		this.range = 65;
-		this.attackPower = 1;
-		this.x = x;
-		this.y = y;
+		this.setAttackPower(1);
+		this.setX(x);
+		this.setY(y);
 		this.cost = 10;
-		this.img = ImageIO.read(new File("basicTower.png"));
+		this.img = "/basicTower.png";
 	}
 	
+	public String getImg() {
+		return img;
+	}
+	
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAttackPower() {
+		return attackPower;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+
+	public void setAttackPower(int attackPower) {
+		this.attackPower = attackPower;
+	}
+
 	public int distance(int x1, int y1,int x2, int y2) {
-		int distance = (int) Math.abs(Math.sqrt((x1 - x2)^2 + (y1 - y2)^2));
+		int distance = (int) Math.abs(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2,2)));
 		return distance;
 	}
 	
-	public Monster selectMonster(List<Monster> monsterList) {
+	public Monster selectMonster(List<Monster> monsterList, Game game) {
 		//Iterate through all monsters and select the one to shoot
 		Monster selectedMonster = null;
 		for(Monster monster: monsterList) {
 			
 			//Check if the monster in range
-			int distance = distance(x, y, monster.x, monster.y);
+			int distance = distance(getX(), getY(), monster.x, monster.y);
 			if(distance <= range) {
 				
 				//Chose which monster to shoot if more than one is in range
 				if(selectedMonster != null) {
 					//Decide which monster is closer to endzone, chose endzone as in demo (440,0)
-					if(distance(monster.x,monster.y,game.endzonex,game.endzoney) < distance(selectedMonster.x,selectedMonster.y,game.endzonex,game.endzoney)) {
+					if(distance(monster.x,monster.y,game.getEndzonex(),game.getEndzoney()) < distance(selectedMonster.x,selectedMonster.y,game.getEndzonex(),game.getEndzoney())) {
 						selectedMonster = monster;
 					}
 				}
@@ -61,24 +91,23 @@ public class BasicTower {
 		return selectedMonster;
 	}
 	
-	public void shoot() {
-		Monster selectedMonster = selectMonster(game.monsterList);
+	public void shoot(Game game) {
+		Monster selectedMonster = selectMonster(game.getMonsterList(), game);
 		//shoot Monster
 				if(selectedMonster != null) {
-					game.monsterList.get(game.monsterList.indexOf(selectedMonster)).hp = selectedMonster.hp - attackPower;
+					game.getMonsterList().get(game.getMonsterList().indexOf(selectedMonster)).hp = selectedMonster.hp - getAttackPower();
 					
 					List<Object> attackPair = new ArrayList<Object>();
 					attackPair.add(this);
 					attackPair.add(selectedMonster);
-					game.attackList.add(attackPair);
+					game.getAttackList().add(attackPair);
 				}
 	}
 	
 	
 	public void upgradeTower() {
-		attackPower = attackPower + 1;
+		setAttackPower(getAttackPower() + 1);
 	}
 	
 	
 }
-
--- src/main/java/towers/IceTower.java
+++ src/main/java/towers/IceTower.java
@@ -2,40 +2,36 @@ package towers;
 import general.*;
 import monsters.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.imageio.ImageIO;
 
 
 public class IceTower extends BasicTower{
 	
-	int slowingFactor;
-	int slowingFrames;
+	private int slowingFactor;
+	private int slowingFrames;
 	
-	public IceTower(int x,int y, Game game) throws IOException{
-		super(x,y,game);
+	public IceTower(int x,int y){
+		super(x,y);
 		this.slowingFactor = 2;
 		this.slowingFrames = 5;
-		this.attackPower = 0;
-		this.img = ImageIO.read(new File("iceTower.png"));
+		this.setAttackPower(0);
+		this.setImg("/iceTower.png");
 	}
 	
 	@Override
-	public void shoot() {
-		Monster selectedMonster = selectMonster(game.monsterList);
+	public void shoot(Game game) {
+		Monster selectedMonster = selectMonster(game.getMonsterList(), game);
 		//shoot Monster
 				if(selectedMonster != null) {
-					game.monsterList.get(game.monsterList.indexOf(selectedMonster)).hp = selectedMonster.hp - attackPower;
-					game.monsterList.get(game.monsterList.indexOf(selectedMonster)).speed = selectedMonster.speed/slowingFactor;
-					game.monsterList.get(game.monsterList.indexOf(selectedMonster)).slowForFrames = slowingFrames;
+					game.getMonsterList().get(game.getMonsterList().indexOf(selectedMonster)).hp = selectedMonster.hp - getAttackPower();
+					game.getMonsterList().get(game.getMonsterList().indexOf(selectedMonster)).speed = selectedMonster.speed/slowingFactor;
+					game.getMonsterList().get(game.getMonsterList().indexOf(selectedMonster)).slowForFrames = slowingFrames;
 					
 					List<Object> attackPair = new ArrayList<Object>();
 					attackPair.add(this);
 					attackPair.add(selectedMonster);
-					game.attackList.add(attackPair);
+					game.getAttackList().add(attackPair);
 				}
 	}
 	
