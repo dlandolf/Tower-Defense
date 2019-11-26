299d0fda2aafab4dfc18b18f11a1288416c22a9f
Tim <tim.engelmann@yahoo.de>
2019-11-26 14:44:25 +0700

    Checked Towers again and resolved some last bugs

--- src/main/java/towers/LaserTower.java
+++ src/main/java/towers/LaserTower.java
@@ -25,16 +25,29 @@ public class LaserTower extends BasicTower{
 	boolean checkIfOnBeam(Monster selectedMonster, Monster monster) {
 	
 		int vx = selectedMonster.getX()-(getX()+20);
-		int vy = selectedMonster.getY()-(getY()+20);
-		double a = (monster.getX()-getX())/vx;
-
-		if(a>0 && (getY()+20) + a * vy - laserWidth < monster.getY() && monster.getY() < (getY()+20) + a * vy + laserWidth) {
-
-			return true;
+		
+		if(vx == 0) {
+			if(monster.getX() == selectedMonster.getX()) {
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
+		
 		else {
-			return false;
+			int vy = selectedMonster.getY()-(getY()+20);
+			double a = (monster.getX()-(getX()+20))/vx;
+
+			if(a>0 && (getY()+20) + a * vy - laserWidth <= monster.getY() && monster.getY() <= (getY()+20) + a * vy + laserWidth) {
+
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
+		
 		
 	}
 	
@@ -42,26 +55,25 @@ public class LaserTower extends BasicTower{
 	public void shoot(Game game) {
 		Monster selectedMonster = selectMonster(game.getMonsterList(), game);
 		//shoot Monster
-				if(selectedMonster != null) {
-					if(game.getResources() >= attackCost) {
-						for(Monster monster: game.getMonsterList()) {
-							if(checkIfOnBeam(selectedMonster, monster)) {
-								game.getMonsterList().get(game.getMonsterList().indexOf(monster)).setHp(monster.getHp() - getAttackPower());
-							}
-						}
+		if(selectedMonster != null) {
+			if(game.getResources() >= attackCost) {
+				for(Monster monster: game.getMonsterList()) {
+					if(checkIfOnBeam(selectedMonster, monster)) {
+						game.getMonsterList().get(game.getMonsterList().indexOf(monster)).setHp(monster.getHp() - getAttackPower());
 					}
-					
-					System.out.println("LaserTower@(" + getX()/40+"," + getY()/40+")"+ " -> " + selectedMonster.getType() + "@(" + (selectedMonster.getX()-20)/40+"," + (selectedMonster.getY()-20)/40+")");
-					game.drawShoot(getX()+20, getY()+20, selectedMonster.getX(), selectedMonster.getY());
-					
-					List<Object> attackPair = new ArrayList<Object>();
-					attackPair.add(this);
-					attackPair.add(selectedMonster);
-					game.getAttackList().add(attackPair);
-					
-					game.setResources(game.getResources() - attackCost);
-			
 				}
+				
+				System.out.println("LaserTower@(" + getX()/40+"," + getY()/40+")"+ " -> " + selectedMonster.getType() + "@(" + (selectedMonster.getX()-20)/40+"," + (selectedMonster.getY()-20)/40+")");
+				game.drawShoot(getX()+20, getY()+20, selectedMonster.getX(), selectedMonster.getY());
+				
+				List<Object> attackPair = new ArrayList<Object>();
+				attackPair.add(this);
+				attackPair.add(selectedMonster);
+				game.getAttackList().add(attackPair);
+				
+				game.setResources(game.getResources() - attackCost);
+			}
+		}
 	}
 	
 	
